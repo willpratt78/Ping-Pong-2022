c531e3921a2bf77376c5bf270d8d659bed70ae26
William Pratt <willpratt78@gmail.com>
2022-05-02 11:27:56 -0700

    all tests currently working

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,9 +2,13 @@ def ping_pong(input)
   x = 0
   array = []
   while (x < input)
-    x = x + 1
-    if (x % 3 === 0) then
+    x += 1
+    if (x % 3 === 0) && (x % 5 === 0)   
+      array.push("ping-pong")
+    elsif (x % 3 === 0)
       array.push("ping")
+    elsif (x % 5 === 0)
+      array.push("pong")
     else 
       array.push(x)
     end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -8,4 +8,13 @@ describe('#ping_pong') do
   it("replaces any number divided by 3 with ping") do
     expect(ping_pong(3)).to(eq([1,2, 'ping']))
   end
+  it("replaces any number divided by 5 with pong") do
+   expect(ping_pong(5)).to(eq([1,2, 'ping', 4, 'pong']))
+  end
+  it("replaces any number divided by 3 & 5 with ping-pong") do
+    expect(ping_pong(15)).to(eq([1,2, 'ping', 4, 'pong', "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
+   end
+   it("replaces any number divided by 3 & 5 with ping-pong") do
+    expect(ping_pong(22)).to(eq([1,2, 'ping', 4, 'pong', "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong", 16, 17, "ping", 19, "pong", "ping", 22]))
+   end
 end
