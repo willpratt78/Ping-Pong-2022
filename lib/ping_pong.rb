5dd361f29cc0d15ca3b933f63041eda15abc8395
William Pratt <willpratt78@gmail.com>
2022-05-02 10:57:01 -0700

    seccond test working

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,7 +3,11 @@ def ping_pong(input)
   array = []
   while (x < input)
     x = x + 1
-    array.push(x)
+    if (x % 3 === 0) then
+      array.push("ping")
+    else 
+      array.push(x)
+    end
   end
-  return array
+return array
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,6 +2,10 @@ require ('rspec')
 require ('ping_pong')
 
 describe('#ping_pong') do
-   it("returns an array of ascending numbers up to the number entered") do
+  it("returns an array of ascending numbers up to the number entered") do
     expect(ping_pong(2)).to(eq([1,2]))
-   end
+  end
+  it("replaces any number divided by 3 with ping") do
+    expect(ping_pong(3)).to(eq([1,2, 'ping']))
+  end
+end
